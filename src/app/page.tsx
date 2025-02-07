f078c90d83f020c14fdb04700a6647bf698444df
James OSullivan <>
2025-02-07 15:09:29 -0500

    Add gitignore, styling and make mobile friendly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { styles } from './styles';
 
 export default function Home() {
   const [advocates, setAdvocates] = useState([]);
@@ -42,50 +43,88 @@ export default function Home() {
   };
 
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
+    <main className={styles.container}>
+      <h1 className={styles.heading}>Solace Advocates</h1>
+      <div className={styles.searchSection}>
+        <p className={styles.searchLabel}>Search</p>
+        <p className={styles.searchTerm}>
           Searching for: <span id="search-term"></span>
         </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+        <div className="flex flex-col sm:flex-row gap-3">
+          <input className={styles.searchInput} onChange={onChange} />
+          <button className={styles.resetButton} onClick={onClick}>
+            Reset Search
+          </button>
+        </div>
       </div>
-      <br />
-      <br />
-      <table>
+
+      {/* Desktop Table */}
+      <table className={styles.table}>
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr>
+            <th className={styles.tableHeader}>First Name</th>
+            <th className={styles.tableHeader}>Last Name</th>
+            <th className={styles.tableHeader}>City</th>
+            <th className={styles.tableHeader}>Degree</th>
+            <th className={styles.tableHeader}>Specialties</th>
+            <th className={styles.tableHeader + " text-right"}>Years of Experience</th>
+            <th className={styles.tableHeader + " text-right"}>Phone Number</th>
+          </tr>
         </thead>
         <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
+          {filteredAdvocates.map((advocate) => (
+            <tr className={styles.tableRow}>
+              <td className={styles.tableCell}>{advocate.firstName}</td>
+              <td className={styles.tableCell}>{advocate.lastName}</td>
+              <td className={styles.tableCell}>{advocate.city}</td>
+              <td className={styles.tableCell}>{advocate.degree}</td>
+              <td className={styles.tableCell}>
+                {advocate.specialties.map((s) => (
+                  <div className={styles.specialtyItem}>{s}</div>
+                ))}
+              </td>
+              <td className={styles.tableCell + " text-right"}>{advocate.yearsOfExperience}</td>
+              <td className={styles.tableCell + " text-right"}>{advocate.phoneNumber}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
+
+      {/* Mobile Cards */}
+      <div className={styles.mobileCards}>
+        {filteredAdvocates.map((advocate) => (
+          <div className={styles.mobileCard}>
+            <div className={styles.mobileCardField}>
+              <span className={styles.mobileCardLabel}>Name</span>
+              <span className={styles.mobileCardValue}>{advocate.firstName} {advocate.lastName}</span>
+            </div>
+            <div className={styles.mobileCardField}>
+              <span className={styles.mobileCardLabel}>City</span>
+              <span className={styles.mobileCardValue}>{advocate.city}</span>
+            </div>
+            <div className={styles.mobileCardField}>
+              <span className={styles.mobileCardLabel}>Degree</span>
+              <span className={styles.mobileCardValue}>{advocate.degree}</span>
+            </div>
+            <div className={styles.mobileCardField}>
+              <span className={styles.mobileCardLabel}>Years of Experience</span>
+              <span className={styles.mobileCardValue}>{advocate.yearsOfExperience}</span>
+            </div>
+            <div className={styles.mobileCardField}>
+              <span className={styles.mobileCardLabel}>Phone</span>
+              <span className={styles.mobileCardValue}>{advocate.phoneNumber}</span>
+            </div>
+            <div className={styles.mobileCardField}>
+              <span className={styles.mobileCardLabel}>Specialties</span>
+              <div className="flex flex-wrap justify-end gap-1">
+                {advocate.specialties.map((s) => (
+                  <div className={styles.specialtyItem}>{s}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
